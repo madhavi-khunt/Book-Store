4deef89ab7600188e85c0ef1158de45e895d4ced
YashDungariya <dungariyayash2002@gmail.com>
2024-10-03 17:41:29 +0530

    web

--- MERN/Client/src/App.jsx
+++ MERN/Client/src/App.jsx
@@ -9,7 +9,9 @@ import ShowBooks from "./assets/pages/ShowBooks";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/" element={<Home></Home>}>
+        Home
+      </Route>
       <Route path="/books/create" element={<CreateBooks></CreateBooks>}></Route>
       <Route path="/books/details/:id" element={<ShowBooks />}></Route>
       <Route path="/books/edit/:id" element={<EditBooks></EditBooks>}></Route>
--- MERN/Client/src/assets/pages/Home.jsx
+++ MERN/Client/src/assets/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
         console.log(error);
         setLoading(false);
       });
-  });
+  }, []);
   return (
     <div className="p-4">
       <div className="flex justify-between items-center">
@@ -39,9 +39,46 @@ function Home() {
             <tr>
               <th className="border border-slate-600 rounded-md">No</th>
               <th className="border border-slate-600 rounded-md">Title</th>
-              <th className="border border-slate-600 rounded-md">No</th>
+              <th className="border border-slate-600 rounded-md max-hd:hidden">
+                Author
+              </th>
+              <th className="border border-slate-600 rounded-md max-hd:hidden">
+                Publish Year
+              </th>
+              <th className="border border-slate-600 rounded-md">Operations</th>
             </tr>
           </thead>
+          <tbody>
+            {books.map((book, index) => {
+              <tr key={book._id} className="h-8">
+                <td className="border border-slate-700 rounded-md text-center">
+                  {index + 1}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center">
+                  {book.title}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center max-hd:hidden">
+                  {book.author}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center max-hd:hidden">
+                  {book.publishYear}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center">
+                  <div className="flex justify-center gap-x-4">
+                    <Link to={`/books/details/${book._id}`}>
+                      <BsInfoCircle className="text-2xl text-green-4xl" />
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className="text-2xl text-yellow-4xl" />
+                    </Link>
+                    <Link to={`/books/details/${book._id}`}>
+                      <MdOutlineDelete className="text-2xl text-red-4xl" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>;
+            })}
+          </tbody>
         </table>
       )}
     </div>
